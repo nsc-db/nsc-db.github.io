9c9a64228cc21aa81959213552d44ac95976b85a
Nageld <Nageld@bxscience.edu>
2018-01-04 23:48:36 -0500

    Added removal of past conversions
    Encoding fix is still WIP remove the -e tag on echo for use atm

--- converter/convert.sh
+++ converter/convert.sh
@@ -1,14 +1,16 @@
 cd files
 rm -rf *save.plist
+rm -rf *.json
 
 for file in *
 do
 	end='.json'
 
-	sed '7!d;q' $file | sed 's/^.\{16\}//g' | sed 's/.\{9\}$//' | python -m json.tool > $file$end
+	output=$(sed '7!d;q' $file | sed 's/^.\{16\}//g' | sed 's/.\{9\}$//' | python -m json.tool)
+	echo -e $output  > $file$end
 	echo $file
 
 done
-rm -rf *.plist
+# rm -rf *.plist
 
 echo "done"
