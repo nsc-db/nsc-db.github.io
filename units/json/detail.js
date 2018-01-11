abc901f0d0167ffeb68a6dc780519fae73da8af9
Yuyya <mjrivera803@gmail.com>
2018-01-11 18:55:19 -0500

    gg

--- units/json/detail.js
+++ units/json/detail.js
@@ -8521,7 +8521,7 @@ window.detail = {
 		skill1type: "Space-Time",
 		skill1speed: "Fast",
 		skill1cost: "100",
-		skill1: "Deal 50,000 fixed damage to one enemy and grant \"Kamui Mode\". Deal 5x more damage while under \"Kamui Mode\". Deal 2x more damage to None Characters. (Max 10x) [Kamui Mode: All allies are immune to single target Skills]",
+		skill1: "Deal 50,000 fixed damage to one enemy and grant \"Kamui Mode\". Deal 5x more damage while under \"Kamui Mode\". Deal 2x more damage to None Characters. (Max 10x) [Kamui Mode: All allies are immune to single target Skills. NOTE: DOES NOT BLOCK FIXED DAMAGE DONT LISTEN TO UNEDUCATED MORONS]",
 
 		skill2name: "Try This",
 		skill2ogname: "これならどうだ",
@@ -8546,7 +8546,7 @@ window.detail = {
 		skill1type: "Space-Time",
 		skill1speed: "Fast",
 		skill1cost: "100",
-		skill1: "Deal 50,000 fixed damage to one enemy and grant \"Kamui Mode\". Deal 5x more damage while under \"Kamui Mode\". Deal 2x more damage to None Characters. (Max 10x) [Kamui Mode: All allies are immune to single target Skills]",
+		skill1: "Deal 50,000 fixed damage to one enemy and grant \"Kamui Mode\". Deal 5x more damage while under \"Kamui Mode\". Deal 2x more damage to None Characters. (Max 10x) [Kamui Mode: All allies are immune to single target Skills. NOTE: DOES NOT BLOCK FIXED DAMAGE DONT LISTEN TO UNEDUCATED MORONS]",
 
 		skill2name: "Try This",
 		skill2ogname: "これならどうだ",
