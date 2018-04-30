8a82296d145e16e8924217e3a94ef371da6876b5
Yuyya <mjrivera803@gmail.com>
2018-04-30 18:54:50 -0400

    oops

--- jp/js/json-gather.js
+++ jp/js/json-gather.js
@@ -16,7 +16,7 @@
 		}
 		//Create Entries in the Table
 		var model = '<tr class="clickable" data-toggle="modal" data-target="#newModal">'
-					+'		<td class="text-center"><img id="icon-table" src="../common/assets/img/units/icons/thumb_' + image + '"height="60px" width="60px" /><div style="display:block">' + data['cardId'] + '</td>'
+					+'		<td class="text-center"><img id="icon-table" src="../common/assets/img/units/icons/thumb_' + image + '"height="60px" width="60px" /><div style="display:none">' + data['cardId'] + '</td>'
 					+'		<td class="text-left"><a href= "view/' + data["cardId"] + '" data-toggle="modal" data-target="#newModal"><strong>' + name + " 【" + data["cardSubName"] + '】</a></td>'
 					+'		<td class="text-center">' + type + '</td>'
 					+'		<td class="text-center affiliation" affiliation="' + affi + '">' + affi + '</td>'
